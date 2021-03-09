ed00a11df0c7dcc1c702440498889c410e42d742
BigBoiiiiWillErklelens <77988690+BigBoiiiiWillErklelens@users.noreply.github.com>
2021-03-09 13:23:10 -0700

    Add files via upload

---
+++ CT#12000.py
@@ -0,0 +1,30 @@
+"""
+Program goals:
+1. Get input from the user (at multiple points)
+2. We need to convert some of this input to INTs from STRs
+3. We need to provide choices to the user
+    a. Add more valurs to a list
+    b. Return a value at a specific index
+
+"""
+
+def mainProgram():
+    myList = []
+    print("Hello, there! Let's work with lists!")
+    print ("Choose from the following options. Type a number below!")
+    choice = input ("1. Add to a list , 2. Return the value at an undex position!   ")
+    if choice == "1":
+        addToList()
+    elif choice == "2":
+        indexValues()
+        
+        
+def addToList():
+    print("Adding to a list! Great choice!")
+    newItem = input("Type an integer here!    ")
+    myList.append(int(newItem))
+
+def indexValues():
+
+if __name__ == "__main__":
+    mainProgram()
