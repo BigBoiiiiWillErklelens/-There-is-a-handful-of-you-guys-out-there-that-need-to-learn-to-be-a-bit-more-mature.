bced14b135c7c99931cac007ca6eacde92f70049
BigBoiiiiWillErklelens <77988690+BigBoiiiiWillErklelens@users.noreply.github.com>
2021-03-18 13:25:11 -0600

    Add files via upload

---
+++ linearSeachIsAss.py
@@ -0,0 +1,73 @@
+"""
+Program goals:
+1. Get input from the user (at multiple points)
+2. We need to convert some of this input to INTs from STRs
+3. We need to provide choices to the user
+    a. Add more valurs to a list
+    b. Return a value at a specific index
+
+"""
+import random
+myList = []
+
+def mainProgram():
+    while True:
+        try:
+            print("Hello, there! Let's work with lists!")
+            print ("Choose from the following options. Type a number below!")
+            choice = input ("""1. Add to a list or
+2. Add a bunchof numbers
+3. Return the value at an index
+4. Random search
+5. Print contents of list
+6. Quit program""")
+            #Add a way to catch bad user responses
+            if choice == "1":
+                addToList()
+            elif choice == "2":
+                addABunch()
+            elif choice == "3":
+                indexValues()
+            elif choice == "4":
+                randomSearch()
+            elif choice == "5":
+                print
+            else:
+                break
+        except:
+            print("You made a whoopsie!")
+        
+        
+def addToList():
+    print("Adding to a list! Great choice!")
+    newItem = input("Type an integer here!    ")
+    myList.append(int(newItem))
+
+def addABunch():
+    print("We're gonna add a bunch of intergers here!")
+    numToAdd = input("How many intergers would you like to add   ")
+    numRange = input("And how high would you like these numbers to go?   ")
+    for x in range(), int(numToAdd):
+        myList.append(random.randint(0, int(numRange)))
+    print("Your list is now complete.")
+
+def indexValues():
+    print("Ohhhh! I heard you need a particular piece of data!")
+    indexPos = input("What index position are your curoius about?")
+    print(myList[int(indexPos)])
+
+def randomSearch():
+    print("RaNdOm SeArCh?!?")
+    print(myList[random.randint(0, len(myList)-1)])
+
+def linearSearch():
+    print("we're gonna check out each item one at a time in your list! NO GOOD.")
+    searchItem = input("What you lookin for, partner?   ")
+    for x in range(len(myList)):
+        if myList[x] == int(searchItem):
+            print("Your item is at index position {}".format(x))
+
+#DunDeRmaIn
+if __name__ == "__main__":
+    mainProgram()
+    #POO
